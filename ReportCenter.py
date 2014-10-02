99cce40704adc1aa64878ca2a1d28187e28fee48
Deryl <arnol229@gmail.com>
2014-10-02 10:55:30 -0500

    LoadSQL now spits out sql command
    Maybe need to define data types for attributes. (is everything being
    interpreted as strings or are numbers ints?)

--- ReportCenter.py
+++ ReportCenter.py
@@ -1,13 +1,13 @@
 import wx
 import csv
 import logging
-import _mysql as SQLdb
+import _mysql
 
 pth_book = "C:\Users\deryarno\Desktop\Reports\data sources\Bookings.csv"
 pth_forecast = "C:\Users\deryarno\Desktop\Reports\data sources\Forecast Bookings.csv"
 
-sqltbl_book = ""
-sqltbl_forecast = ""
+sqltbl_book = "book"
+sqltbl_forecast = "fcp"
 
 OPTIONS_ID = wx.NewId()
 
@@ -36,20 +36,20 @@ class Frame(wx.Frame):
 
     def ProcessBookings(self, event):
         logging.error("processing bookings...")
-        try:
-            self.ImportCSV(pth_book,sqltbl_book)
-        except Exception as e:
-            logging.error("Error: " + str(e))
+        #try:
+        self.ImportCSV(pth_book,sqltbl_book)
+        #except Exception as e:
+        #    logging.error("Error: " + str(e))
 
-        try:
-            self.ImportCSV(pth_forecast,sqltbl_forecast)
-        except Exception as e:
-            logging.error("Error: " + str(e))
+        #try:
+        self.ImportCSV(pth_forecast,sqltbl_forecast)
+        #except Exception as e:
+        #    logging.error("Error: " + str(e))
 
             
 
     def ImportCSV(self,pth,table):
-        logging.error('Importing CSV at: ' + pth + "to table: " + table)
+        logging.error("Importing CSV at '" + pth + "' to table '" + table + "'")
         with open(pth,'rb') as csvfile:
             reader = csv.reader(csvfile,delimiter=',')
             columns = next(reader)
@@ -62,19 +62,50 @@ class Frame(wx.Frame):
                 for i in range(len(columns)):
                     setattr(row,columns[i],entry[i])
                 entries.append(row)
-                if len(entries) == 1000:
+                if len(entries) == 10:
                     self.LoadSQL(entries,table)
                     entries = []
     
             print "done adding " + str(total) + " entries"
     
     def LoadSQL(self,entries,table):
-        con = SQLdb.connect('localhost', 'ReportCenter', 'ciscoiotg')
+        #con = _mysql.connect()
+#
+        ##clean table
+        #cmd = "DELETE * FROM " + table
+        #con.executescript(cmd)
 
-        con.query("SELECT VERSION()")
-        result = con.use_result()
+        #add entries to table
+        cmd = "INSERT INTO " + table + "("
+        sample = entries[0]
+        try:
+            attributes = [a for a in dir(sample) if not a.startswith('__') and not callable(getattr(sample,a))]
+        except Exception as e:
+            logging.error("haha didnt work")
+        for col in attributes:
+            cmd += col
+            if col != attributes[-1]:
+                cmd += ","
 
-        print result
+        cmd += ") VALUES "
+        for entry in entries:
+            cmd += "("
+            for col in attributes:
+                cmd += str(getattr(entry,str(col)))
+                if col != attributes[-1]:
+                    cmd += ","
+                else:
+                    cmd += ")"
+                    if entry != entries[-1]:
+                        cmd += ','
+
+        logging.error("YOUR SQL COMMAND: ")
+        logging.error(cmd)
+
+        #con.query("SELECT VERSION()")
+        #result = con.use_result()
+#
+        #print result
 
     def OnExit(self, event):
         self.Destroy()
@@ -84,6 +115,7 @@ class Frame(wx.Frame):
 
     class Entry(object):
         pass
+
 logging.error(__name__)
 if __name__ == '__main__':
     logging.error("welcome to Report Center")
